00d726a05cbb5573d428cab4425846946b16e520
Philip <philip.lanphar@gmail.com>
2017-12-11 08:52:50 -0700

    Added ball

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,19 @@ function pong(): any {
     ctx = canvas.getContext("2d");
     gameLoop();
 
+    function ball() {
+        ctx.beginPath();
+        ctx.fillStyle = "#ffffff";
+        ctx.arc(100, 100, 10, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
     function gameLoop() {
         requestAnimationFrame(gameLoop);
         ctx.fillStyle = "black";
         ctx.fillRect(0, 0, 1500, 720);
-        ctx.beginPath();
+        ball();
+
     }
 }
 
