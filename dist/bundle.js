d4431335113aa78e835df2a19fe266d523754469
Philip Lanphar <philip.lanphar@gmail.com>
2017-12-11 22:17:08 -0700

    Changed speed of ball and resized canvas

--- dist/bundle.js
+++ dist/bundle.js
@@ -72,28 +72,45 @@ function pong() {
     var ctx;
     canvas = document.getElementById('pong');
     ctx = canvas.getContext("2d");
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
     var canvasWidth = canvas.width;
     var canvasHeight = canvas.height;
-    gameLoop();
     var x = 10;
     var y = canvasHeight / 2;
-    var a = 15;
-    var b = 15;
+    var a = 10;
+    var b = 10;
+    var paddleX = 0;
+    var paddleY = canvasHeight / 2 - 100;
+    var direction = 1;
+    var score = 0;
+    gameLoop();
     function ballDirection() {
-        if (x >= canvasWidth) {
-            a = -a;
-        }
-        else if (x < 5) {
+        if (x <= 5 && (y > paddleY && y < paddleY + 200)) {
             a = Math.abs(a);
+            b = b * direction;
+            score = score + 1;
         }
-        if (y >= canvasHeight) {
-            b = -b;
-        }
-        else if (y < 5) {
-            b = Math.abs(b);
+        else {
+            if (x >= canvasWidth) {
+                a = -a;
+            }
+            else if (x < 5) {
+                a = Math.abs(a);
+            }
+            if (y >= canvasHeight) {
+                b = -b;
+            }
+            else if (y < 5) {
+                b = Math.abs(b);
+            }
         }
         x = x + a;
         y = y + b;
+        ctx.font = "50px Comic Sans MS";
+        ctx.fillStyle = "#ffffff";
+        ctx.textAlign = "center";
+        ctx.fillText(score.toString(), canvas.width / 2 - 100, 100);
     }
     function ballDraw() {
         ballDirection();
@@ -102,27 +119,24 @@ function pong() {
         ctx.arc(x, y, 10, 0, 2 * Math.PI);
         ctx.fill();
     }
-    var paddleX = 0;
-    var paddleY = canvasHeight / 2 - 100;
     function paddleDraw() {
         ctx.fillStyle = "#ffffff";
         ctx.fillRect(paddleX, paddleY, 15, 200);
-        paddleMove();
     }
-    function paddleMove() {
-        document.addEventListener('keydown', function (event) {
-            if (event.keyCode === 38 && paddleY > 0) {
-                paddleY = paddleY - 0.10;
-            }
-            else if (event.keyCode === 40 && paddleY < canvasHeight - 200) {
-                paddleY = paddleY + 0.10;
-            }
-        }, false);
-    }
+    document.addEventListener('keydown', function (event) {
+        if (event.keyCode === 38 && paddleY > 0) {
+            paddleY = paddleY - 50;
+            direction = -1;
+        }
+        else if (event.keyCode === 40 && paddleY < canvasHeight - 200) {
+            paddleY = paddleY + 50;
+            direction = 1;
+        }
+    }, false);
     function gameLoop() {
         requestAnimationFrame(gameLoop);
         ctx.fillStyle = "black";
-        ctx.fillRect(0, 0, 1500, 700);
+        ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
         ballDraw();
         paddleDraw();
         ctx.beginPath();
--- dist/src/index.js
+++ dist/src/index.js
@@ -1,77 +1,69 @@
-function pong(): any {
-    let canvas: HTMLCanvasElement;
-    let ctx: CanvasRenderingContext2D;
-    canvas = <HTMLCanvasElement>document.getElementById('pong');
+function pong() {
+    var canvas;
+    var ctx;
+    canvas = document.getElementById('pong');
     ctx = canvas.getContext("2d");
-    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
-    let canvasWidth: number = canvas.width;
-    let canvasHeight: number = canvas.height;
-    let x: number = 10;
-    let y: number = canvasHeight / 2;
-    let a: number = 10;
-    let b: number = 10;
-    let paddleX: number = 0;
-    let paddleY: number = canvasHeight/2 -100;
-    let direction: number = 1;
-    let score: number = 0;
+    var canvasWidth = canvas.width;
+    var canvasHeight = canvas.height;
+    var x = 10;
+    var y = canvasHeight / 2;
+    var a = 10;
+    var b = 10;
+    var paddleX = 0;
+    var paddleY = canvasHeight / 2 - 100;
+    var direction = 1;
+    var score = 0;
     gameLoop();
-
-
-    function ballDirection(): void {
-
+    function ballDirection() {
         if (x <= 5 && (y > paddleY && y < paddleY + 200)) {
             a = Math.abs(a);
             b = b * direction;
             score = score + 1;
-        } else {
+        }
+        else {
             if (x >= canvasWidth) {
                 a = -a;
-            } else if (x < 5) {
+            }
+            else if (x < 5) {
                 a = Math.abs(a);
             }
-
             if (y >= canvasHeight) {
                 b = -b;
-            } else if (y < 5) {
+            }
+            else if (y < 5) {
                 b = Math.abs(b);
             }
         }
-
         x = x + a;
         y = y + b;
-
         ctx.font = "50px Comic Sans MS";
         ctx.fillStyle = "#ffffff";
         ctx.textAlign = "center";
-        ctx.fillText(score.toString(), canvas.width/2 - 100, 100);
-
+        ctx.fillText(score.toString(), canvas.width / 2 - 100, 100);
     }
-
-    function ballDraw(): void {
+    function ballDraw() {
         ballDirection();
         ctx.beginPath();
         ctx.fillStyle = "#ffffff";
         ctx.arc(x, y, 10, 0, 2 * Math.PI);
         ctx.fill();
     }
-
-    function paddleDraw():void {
+    function paddleDraw() {
         ctx.fillStyle = "#ffffff";
-        ctx.fillRect(paddleX,paddleY,15,200);
+        ctx.fillRect(paddleX, paddleY, 15, 200);
     }
-
-    document.addEventListener('keydown', (event) => {
+    document.addEventListener('keydown', function (event) {
         if (event.keyCode === 38 && paddleY > 0) {
             paddleY = paddleY - 50;
             direction = -1;
         }
-        else if (event.keyCode === 40 && paddleY < canvasHeight-200) {
+        else if (event.keyCode === 40 && paddleY < canvasHeight - 200) {
             paddleY = paddleY + 50;
             direction = 1;
         }
-    }, false)
-
+    }, false);
     function gameLoop() {
         requestAnimationFrame(gameLoop);
         ctx.fillStyle = "black";
@@ -79,17 +71,12 @@ function pong(): any {
         ballDraw();
         paddleDraw();
         ctx.beginPath();
-        ctx.moveTo(canvasWidth/2,0);
-        ctx.lineTo(canvasWidth/2,canvasHeight);
+        ctx.moveTo(canvasWidth / 2, 0);
+        ctx.lineTo(canvasWidth / 2, canvasHeight);
         ctx.strokeStyle = '#ffffff';
         ctx.stroke();
     }
 }
-
-window.onload = () => {
+window.onload = function () {
     pong();
 };
-
-
-
-
